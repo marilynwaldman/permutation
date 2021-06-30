bda8e7e2c6a31d3a92972787a5004376d63a29fa
MarilynWaldman <mdwaldman22@gmail.com>
2021-06-30 09:55:40 -0600

    first push

---
+++ src/permutation.py
@@ -0,0 +1,13 @@
+def permutation(n):
+    fact = 1
+    # compute n!
+    return fact
+
+def n_take_k(n,k):
+    # find n take k
+    return k
+
+
+if __name__ == "__main__":
+        print(permutation(5))
+        print(n_take_k(5,3))        
